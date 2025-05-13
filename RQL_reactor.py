6c99c486aaaeac0f12e5e001c7ef809197ed505f
sksiezak <z.a.ksiezak-1@tudelft.student.nl>
2025-05-13 16:32:27 +0200

    code cleanup

--- RQL_reactor.py
+++ RQL_reactor.py
@@ -2,44 +2,46 @@ import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#------Mechanism Selection------
 mechanism_1 = 'gri30.yaml'  # Mechanism for H2 combustion with NOx
 mechanism_2 = 'CapursoMechanism.yaml'  # Mechanism for H2 combustion with NOx
 mechanism_3 = 'H2_pNOX_15_94_TC.yaml'  # Mechanism for H2 combustion with NOx
 
+#------Parameters------
+P = 12.1 * ct.one_atm         # Pressure [Pa]
+T_initial = 603.25            # Initial temperature [K]
 gas = ct.Solution(mechanism_1)
-gas.TP = 630.25, 12.1 * ct.one_atm
+gas.TP = T_initial, P
 gas.set_equivalence_ratio(2.5, 'H2', 'O2:2.1, N2:7.9')
 gas_copy = gas
 species_to_track = ['H2', 'O2', 'H2O', 'NO', 'NO2']
 species_data = {sp: [] for sp in species_to_track if sp in gas.species_names}
 
+#------Reactor Setup------
 reactor = ct.IdealGasConstPressureReactor(gas)
 sim = ct.ReactorNet([reactor])
 
-# Set up for data collection
+#------Iteration Parameters------
 time = 0.0
 t_end = 0.01
-dt = 5e-6
+dt = 1e-6
 
 times = []
 temps = []
 
-# Parameters for spark
-IDT = 0.002  # Time when spark occurs [s]
+IDT = 0.002  # ignition delay time [s]
 volume = reactor.volume
 
+#------Ignition Temperature------
 gas_copy.equilibrate('HP')
 T_igition = gas_copy.T  # Set ignition temperature slightly above equilibrium temperature
 
-# Loop over time
+#------Simulation Loop------
 while time < t_end:
     time += dt
 
     # Simulate spark by increasing temperature (energy deposition)
     if abs(time - IDT) < dt:
-        T_current = reactor.T
-        mass = reactor.mass
         gas.TP = T_igition, reactor.thermo.P # Set the gas to the ignition temperature
         reactor.syncState()
         
@@ -50,6 +52,7 @@ while time < t_end:
         species_data[sp].append(reactor.thermo[sp].X[0])
 
 
+#------Results------
 print(f'Ignition temperature: {T_igition:.2f} K')
 print(f'Initial temp: ', temps[0], '; Final temp: ', temps[-1])
 print("\nFinal Species Content (mole fractions):")
@@ -64,7 +67,7 @@ plt.subplot(2, 1, 1)
 plt.plot(times, temps, color='red', label='Temperature [K]')
 plt.xlabel('Time [s]')
 plt.ylabel('Temperature [K]')
-plt.title('Spark Ignition via Energy Deposition')
+plt.title('Temperature Rise Over Time')
 plt.grid(True)
 plt.legend()
 
