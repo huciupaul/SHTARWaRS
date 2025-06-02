66cd0df0376761c990c8646b6186a1dc72f885a7
huciupaul <huciupaul3@gmail.com>
2025-06-02 14:25:44 +0200

    push2

--- eps_sizing.py
+++ eps_sizing.py
@@ -15,5 +15,19 @@ def fc_power_needed(P_req):
     """Calculate the total power needed from the fuel cell."""
     return P_req / (0.97**2)
 
-def eps_sizing(P_req):
+def motor_sizing(lambd, P_req):
     """Calculate the EPS sizing parameters."""
+    c1 = 0.0004
+    c2 = 0.0008
+    c3 = 1.0744
+    hs = 0.0366
+    P_req = P_req / 2
+    r = (lambd * P_req)**(1/3) * c1
+    l = (lambd * P_req)**(1/3) * c2 * 1/lambd
+    hy = r * c3
+    r_tot = r + hs + hy
+    return r_tot, l
+
+def motor_volume(radius, length):
+    """Calculate the volume of the EPS."""
+    return np.pi * radius**2 * length
