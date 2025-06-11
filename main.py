aa64b7afbdde291b78bf055e2fdb20c17a3fe05f
huciupaul <huciupaul3@gmail.com>
2025-06-11 08:48:54 +0200

    WRapper ready without TMS

--- main.py
+++ main.py
@@ -11,45 +11,50 @@ import numpy as np
 from global_constants import * # Import global constants
 import pickle
 
+# Local imports
 from eps.eps_sizing import eps_main  # Import EPS sizing function
 from fpp.flight import fpp_main  # Import FPP sizing function
 from storage.tank import main_storage  # Import storage sizing function
 
-# Internal imports
-#...
 
 
-def main():
+def main(minimum, maximum, no_of_splits, max_iter):
     # Check if the output directory exists, if not create it
     output_dir = "data/logs"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Initialize a nested defaultdict that automatically creates new levels
 
-    # Define power split array
-    # power_splits = np.linspace(args.min, args.max, args.n_splits+1)
-    # fc_toga_percentages = np.linspace(0, 1, 200)  # Percentage of TOGA power for the fuel cell
+    # Define the power splits and fuel cell percentages
+    power_splits = np.linspace(minimum, maximum, no_of_splits)  # 10 splits from 0 to 1
+    fc_toga_percentages = np.linspace(minimum, maximum, no_of_splits)  # Example percentages of TOGA power for the fuel cell
+    fc_cruise_percentages = np.linspace(minimum, maximum, no_of_splits)  # Example percentages of CRUISE power for the fuel cell
 
-    power_splits = np.array([0.2])  # Example: 10 splits from 0 to 1
-    fc_toga_percentages = np.array([0.2])  # Example percentages of TOGA power for the fuel cell
+    result_tensor = np.zeros((
+    len(power_splits),
+    len(fc_toga_percentages),
+    len(fc_cruise_percentages),
+    14  # Length of the tensor
+))
 
-
-    for split in power_splits:
+    for i_split, split in enumerate(power_splits):
 
         m_eps, Qdot_eps, V_elmo = eps_main(split)
 
-        for fc_toga_percentage in fc_toga_percentages:
+        for i_toga, fc_toga_percentage in enumerate(fc_toga_percentages):
 
-            for fc_cruise_percentage in fc_toga_percentages:
+            for i_cruise, fc_cruise_percentage in enumerate(fc_cruise_percentages):
 
             
                 MTOW = Beechcraft_1900D['OEW'] # Original Maximum Take-Off Weight  OEW
                 delta_ap, c_d_rad = 0.0, 0.0
                 m_eps_prev, m_fc_tms_prev, m_h2_prev, m_sto_prev, m_cargo_prev = 0.0, 326.63, 315.39, 141.38, 714.05 # from midterm
                 MTOW += (m_eps + m_fc_tms_prev + m_h2_prev + m_sto_prev)  # Initial MTOW update
-                for i in range(2):
-                    print(MTOW)
+                for i in range(max_iter):
+                    if ((MTOW-MTOW_prev)/MTOW_prev) < 0.01 and i > 0:
+                        print(f"Converged after {i} iterations with MTOW: {MTOW:.2f} kg")
+                        break
+                    #print(MTOW)
                     if i > 0:
                         # Update the values based on the previous iteration
                         m_eps_prev = m_eps
@@ -71,13 +76,25 @@ def main():
                     m_cargo = 0
                     
                     #MTOW update
+                    MTOW_prev = MTOW
                     MTOW += ((m_eps - m_eps_prev) + (m_h2 - m_h2_prev) + (m_sto - m_sto_prev) + (m_tms_front + m_tms_aft + m_tms_mid + m_fc - m_fc_tms_prev))
                 tensor = np.array([m_eps, m_fc, m_h2, m_sto,V_fc, V_sto, V_elmo, MTOW, length_sto, diameter_sto, m_cargo, m_tms_front, m_tms_aft, m_tms_mid])
+
+                result_tensor[i_split, i_toga, i_cruise, :] = tensor
+
+    with open("data/logs/result_tensor.pkl", "wb") as f:
+        pickle.dump(result_tensor, f)
 
     return None       
     
 
 #main()
+
+# with open("data/logs/result_tensor.pkl", "rb") as f:
+#     loaded_tensor = pickle.load(f)
+
+# print(loaded_tensor[0,0,1,:])
+# print(loaded_tensor.shape)  # Print the shape of the tensor for verification
 
 
 # if __name__=="__main__":
