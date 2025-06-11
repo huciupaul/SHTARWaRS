7d416e9f40845ede1e71be60ad7263e2417dd1b6
huciupaul <huciupaul3@gmail.com>
2025-06-11 08:54:56 +0200

    Comments added

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ def main(minimum, maximum, no_of_splits, max_iter):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-
     # Define the power splits and fuel cell percentages
     power_splits = np.linspace(minimum, maximum, no_of_splits)  # 10 splits from 0 to 1
     fc_toga_percentages = np.linspace(minimum, maximum, no_of_splits)  # Example percentages of TOGA power for the fuel cell
     fc_cruise_percentages = np.linspace(minimum, maximum, no_of_splits)  # Example percentages of CRUISE power for the fuel cell
 
+    # Initialize the result tensor with zeros
     result_tensor = np.zeros((
     len(power_splits),
     len(fc_toga_percentages),
@@ -38,23 +38,26 @@ def main(minimum, maximum, no_of_splits, max_iter):
 ))
 
     for i_split, split in enumerate(power_splits):
-
+        
+        # EPS
         m_eps, Qdot_eps, V_elmo = eps_main(split)
 
         for i_toga, fc_toga_percentage in enumerate(fc_toga_percentages):
 
             for i_cruise, fc_cruise_percentage in enumerate(fc_cruise_percentages):
 
-            
-                MTOW = Beechcraft_1900D['OEW'] # Original Maximum Take-Off Weight  OEW
+                # Initialize variables for the first iteration of convergence
+                MTOW = Beechcraft_1900D['OEW'] # Original OEW
                 delta_ap, c_d_rad = 0.0, 0.0
                 m_eps_prev, m_fc_tms_prev, m_h2_prev, m_sto_prev, m_cargo_prev = 0.0, 326.63, 315.39, 141.38, 714.05 # from midterm
-                MTOW += (m_eps + m_fc_tms_prev + m_h2_prev + m_sto_prev)  # Initial MTOW update
+                MTOW += (m_eps + m_fc_tms_prev + m_h2_prev + m_sto_prev)  # Initial MTOW
+
+                # Convergence of MTOW, stop if the change is less than 1% of the previous MTOW or reached max number of iterations
                 for i in range(max_iter):
                     if ((MTOW-MTOW_prev)/MTOW_prev) < 0.01 and i > 0:
                         print(f"Converged after {i} iterations with MTOW: {MTOW:.2f} kg")
                         break
-                    #print(MTOW)
+                    
                     if i > 0:
                         # Update the values based on the previous iteration
                         m_eps_prev = m_eps
@@ -63,10 +66,12 @@ def main(minimum, maximum, no_of_splits, max_iter):
                         m_sto_prev = m_sto
                         m_cargo_prev = m_cargo
                     
-                    # Perform optimization steps here
+                    # FPP
                     TMS_inputs, m_h2, FC_outputs, mission_profile = fpp_main(split, fc_toga_percentage, fc_cruise_percentage, MTOW, c_d_rad, delta_ap, 0.1)
                     m_fc = FC_outputs['m_fc']
                     V_fc = FC_outputs['V_fc']
+
+                    # Storage
                     m_sto, V_sto, t1, dv, t2, length_sto, diameter_sto = main_storage(m_h2)
 
                     # Update delta_AP, c_D_rad based on the TMS code and get:
@@ -78,10 +83,12 @@ def main(minimum, maximum, no_of_splits, max_iter):
                     #MTOW update
                     MTOW_prev = MTOW
                     MTOW += ((m_eps - m_eps_prev) + (m_h2 - m_h2_prev) + (m_sto - m_sto_prev) + (m_tms_front + m_tms_aft + m_tms_mid + m_fc - m_fc_tms_prev))
-                tensor = np.array([m_eps, m_fc, m_h2, m_sto,V_fc, V_sto, V_elmo, MTOW, length_sto, diameter_sto, m_cargo, m_tms_front, m_tms_aft, m_tms_mid])
 
+                # After convergence, store the results in the tensor, then in the 4D tensor
+                tensor = np.array([m_eps, m_fc, m_h2, m_sto,V_fc, V_sto, V_elmo, MTOW, length_sto, diameter_sto, m_cargo, m_tms_front, m_tms_aft, m_tms_mid])
                 result_tensor[i_split, i_toga, i_cruise, :] = tensor
 
+    # Save the result tensor to a pickle file
     with open("data/logs/result_tensor.pkl", "wb") as f:
         pickle.dump(result_tensor, f)
 
