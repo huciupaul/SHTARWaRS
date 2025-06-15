705f9687785df9e57f9a165f83d12648de50e1d4
sbcox <stefanbennettcox@gmail.com>
2025-06-15 20:23:23 +0200

    added cost calculation for fuel cell

--- cost.py
+++ cost.py
@@ -1,12 +1,11 @@
 from global_constants import *
 
 
-def calc_cost(P_fc_tot, P_fc_used, P_eps, m_fc, m_sto, m_h2_nom, m_h2):
+def calc_cost(fc_cost, P_eps, m_sto, m_h2_nom, m_h2):
     """
     Calculate the cost of the aircraft and its fuel over its lifetime.
     Pilot costs are not included.
-    :param P_fc_tot: 100% throttle power of fuel cell [kW]
-    :param P_fc_used: max power used by the fuel cell [kW]
+    :param fc_cost: cost of the fuel cell over lifetime [EUR]
     :param P_eps: max power used by the electric propulsion system [kW]
     :param m_fc: mass of the fuel cell [kg]
     :param m_sto: mass of the storage system [kg]
@@ -15,7 +14,6 @@ def calc_cost(P_fc_tot, P_fc_used, P_eps, m_fc, m_sto, m_h2_nom, m_h2):
 
     :return: total cost of the aircraft and its fuel over its lifetime [EUR]
     """
-    fc_cost = P_fc_used * FC_cost_no_bop * 3 + P_fc_tot * (FC_cost - FC_cost_no_bop) + m_fc * FC_disposal_cost + FC_maint_cost * P_fc_used
     sto_cost = m_h2 * Sto_cost + m_sto * Sto_disposal_cost
     eps_cost = P_eps * EPS_cost
     H2_cost = m_h2_nom * flight_lifetime * H2_cost
